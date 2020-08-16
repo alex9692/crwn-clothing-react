f8026dd6c8b0f190d3bc74e16b83802224b376ce
alex9692 <battle253@gmail.com>
2020-08-16 14:35:00 +0530

    updated our menu-items component to navigate with the help of withrouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import Homepage from "./pages/homepage/homepage.component";
+
 import "./App.css";
+
+const HatsPage = () => {
+  return <h1>HATS PAGE</h1>;
+};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route path="/hats" component={HatsPage} />
+        <Route path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-export default ({ title, imageUrl, size }) => {
-  return (
-    <div className={`${size ? size : ""} menu-item`}>
+export default withRouter(
+  ({ title, imageUrl, size, linkUrl, history, match }) => {
+    return (
       <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      ></div>
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
+        className={`${size ? size : ""} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
+      >
+        <div
+          className="background-image"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        ></div>
+        <div className="content">
+          <h1 className="title">{title.toUpperCase()}</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
